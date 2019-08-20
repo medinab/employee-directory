4850042bf60e57d1a5ce088becae55146550fa54
medinab <medina1brandon@gmail.com>
2019-08-19 22:05:16 -0400

    refactor app to a functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import {Col, Row, Container} from 'react-bootstrap'
 import './App.css';
 import Home from './components/Home';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedEmployee: [],
-      employees: [],
-      filteredEmployees: []
-    }
-  }
+const App = (props) => {
+  const [selectedEmployee, setSelectedEmployee] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
 
-  async componentDidMount() {
-    await this.setEmployees();
-  }
+  useEffect(() => {
+    (async () => {
+      try {
+        await fetchEmployees();
+      } catch(error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
-  setEmployees = async () => {
+  const fetchEmployees = async () => {
     const response = await fetch('/api/employee');
     const data = await response.json();
-    this.setState({employees: data});
-    this.setState({filteredEmployees: data});
+    setEmployees(data);
+    setFilteredEmployees(data);
+    setSelectedEmployee(employees);
   }
 
-  setSelectedEmployee = () => {
-    this.setState({selectedEmployee:this.state.employees});
-  }
-
-  filterEmployees = (type, searchValue) => {
-    return this.state.employees.filter((employee) => {
+  const filterEmployees = (type, searchValue) => {
+    return employees.filter((employee) => {
       if (type === 'location') {
         if (employee.location[type].toLowerCase().includes(searchValue.toLowerCase())) {
           return true;
@@ -44,29 +42,89 @@ class App extends Component {
     });
   }
 
-  onSearch = (e) => {
-    this.setState({
-      filteredEmployees: this.filterEmployees('name', e.target.value)
-    })
+  const onSearch = (e) => {
+    setFilteredEmployees(filterEmployees('name', e.target.value));
   }
 
-  onClickEmployee = (employee) => {
-    this.setState({
-      selectedEmployee: {name: employee.name.first, email: employee.email}
-    });
-  }
+  // const onClickEmployee = (employee) => {
+  //   selectedEmployee({name: employee.name.first, email: employee.email});
+  // }
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Route exact path='/' render={()=><Home data={this.state.filteredEmployees} employees={this.state.employees} onSearch={this.onSearch} /> } />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Route exact path='/' render={()=><Home data={filteredEmployees} employees={employees} onSearch={onSearch} /> } />
+        </Col>
+      </Row>
+    </Container> 
+  )
 }
+
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       selectedEmployee: [],
+//       employees: [],
+//       filteredEmployees: []
+//     }
+//   }
+
+//   async componentDidMount() {
+//     await this.setEmployees();
+//   }
+
+//   setEmployees = async () => {
+//     const response = await fetch('/api/employee');
+//     const data = await response.json();
+//     this.setState({employees: data});
+//     this.setState({filteredEmployees: data});
+//   }
+
+//   setSelectedEmployee = () => {
+//     this.setState({selectedEmployee:this.state.employees});
+//   }
+
+//   filterEmployees = (type, searchValue) => {
+//     return this.state.employees.filter((employee) => {
+//       if (type === 'location') {
+//         if (employee.location[type].toLowerCase().includes(searchValue.toLowerCase())) {
+//           return true;
+//         }
+//       } else {
+//         if (employee[type].toLowerCase().includes(searchValue.toLowerCase())) {
+//           return true;
+//         }
+//       }
+//       return false;
+//     });
+//   }
+
+//   onSearch = (e) => {
+//     this.setState({
+//       filteredEmployees: this.filterEmployees('name', e.target.value)
+//     })
+//   }
+
+//   onClickEmployee = (employee) => {
+//     this.setState({
+//       selectedEmployee: {name: employee.name.first, email: employee.email}
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <Container>
+//         <Row>
+//           <Col>
+//             <Route exact path='/' render={()=><Home data={this.state.filteredEmployees} employees={this.state.employees} onSearch={this.onSearch} /> } />
+//           </Col>
+//         </Row>
+//       </Container>
+//     );
+//   }
+// }
 
 export default App;
